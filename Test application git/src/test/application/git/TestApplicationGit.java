2a5a323f61365acd1bc9b7e76672413d3340c552
Mihai Cucuianu <mihaicucuianu@gmail.com>
2018-04-20 16:29:52 +0300

    Test git repository

---
+++ Test application git/src/test/application/git/TestApplicationGit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.application.git;
+
+/**
+ *
+ * @author MiIke Window
+ */
+public class TestApplicationGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hei ce faci");
+    }
+    
+}
